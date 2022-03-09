d59d05f564c682c78281fd3db290e5bb7fddde32
Rosemary Ku <rosemary.ku@hotmail.com>
2022-03-08 21:08:08 -0500

    admin dashboard counts

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,7 @@ class Admin::DashboardController < ApplicationController
     http_basic_authenticate_with name: ENV['USERNAME'], password: ENV['PASSWORD']
     
   def show
+    @num_of_products = Product.count
+    @num_of_categories = Category.count
   end
 end
