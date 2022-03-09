b2c13d2ff16fba36ca4eaa18846d07689e739862
Rosemary Ku <rosemary.ku@hotmail.com>
2022-03-08 21:53:55 -0500

    create about page

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
 
   root to: 'products#index'
 
+  
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
+  resources :about, only: [:index]
 
   resource :cart, only: [:show] do
     post   :add_item
@@ -11,10 +13,13 @@ Rails.application.routes.draw do
   end
 
   resources :orders, only: [:create, :show]
+  
 
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
+
+
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
